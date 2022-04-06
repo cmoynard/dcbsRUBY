f1b771915b446f3b0daeddc982fd5efff4f56e86
cmoynard <cmoynard.etude@gmail.com>
2022-04-06 07:39:42 -0700

    formulaire root/ fini

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :admins, :skip => [:registrations]
 
-  root to: "savforms#home"
+  root to: "savforms#new"
 
   get "/accadmin", to: "fournis#accueil"
 
